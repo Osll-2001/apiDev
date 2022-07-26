5859168c919887438a5a4b4949c949a4bab18c1b
octavio <octaviosuriellopezlopez@gmail.com>
2022-07-25 20:09:22 -0500

    fix:se arreglo el error de sintaxis del modelo bands

--- src/models/bands.model.js
+++ src/models/bands.model.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose');
 const Bands=mongoose.model('Bands',{
     name:{ type:String, required:true},
     genre:{type:String, required:true},
-    startYear:{type:number},
+    startYear:{type:Number},
 
 })
 
