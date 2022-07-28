0700908d64e60a0ff749b67b44d3fb3bfdfcc4b6
octavio <octaviosuriellopezlopez@gmail.com>
2022-07-27 19:46:52 -0500

    feat:se configuro para el problema de cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express=require('express');
 const app=express();
 const mongoose=require('mongoose');
-const port=8080;
+const cors=require('cors');
+
+const port=process.env.PORT || 8080;
 
 const routerApi=require('./routes/main.router');
 
@@ -14,5 +16,22 @@ app.listen(port,()=>{
 })
 
 app.use(express.json());
+
+// CORS
+const whitelist = ['*'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('No permitido por tema de CORS'));
+    }
+  }
+};
+app.use(cors(corsOptions));
+
+app.get('/',(req,res)=>{
+    res.send('Api Music V.1.');
+})
 
 routerApi(app);
